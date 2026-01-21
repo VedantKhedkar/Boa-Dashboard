0dc3ab1aaa7bac25e5bdf73287a35e11b582b2b9
Vedant Khedkar <vedantkhedkar4@gmail.com>
2026-01-21 12:17:02 +0530

    v

--- BOA/vite.config.js
+++ BOA/vite.config.js
@@ -1,12 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
+import { fileURLToPath } from 'url'
+
+// Define __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Change from '/admin/' to '/'
+  base: '/', 
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      // This allows you to use "@" to refer to your "src" folder
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     port: 5173,
+    host: true,
   },
 })
